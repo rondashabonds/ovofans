e46304a064b1111dad5f3da2e7101692a142a971
Rondasha Bonds <rondashabonds@gmail.com>
2025-10-26 13:27:52 -0400

    up

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles/styles.css"; // global base styles
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
